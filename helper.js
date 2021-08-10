4186f553f9c1ba3fab7a3fd3980da4e923485d0c
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2021-08-10 11:40:32 +0700

    refine: make pagination more smooth with delay

--- helper.js
+++ helper.js
@@ -1,31 +1,30 @@
 $(function () {
-    window.on_all_image_loaded = ({ envs = [document], callback = null }) => {
-        let done_count = 0
+  window.on_all_image_loaded = ({ envs = [document], callback = null }) => {
+    let done_count = 0
 
-        const add_done = () => {
-            done_count += 1
-            if (done_count === envs.length) {
-                callback()
-            }
-        }
-
-        for (const env of envs) {
-            var $imgs = $('img', env), total = $imgs.length, sum = 0
-            if (total == 0) {
-                add_done()
-            } else {
-                $imgs.each(function() {
-                    var img = new Image();
-                    img.src = $(this).attr('src')
-                    img.onload = function() {
-                        sum++;
-                        if (sum === total) {
-                            add_done()
-                        }
-                    }
-                })
-            }
-        }
-        
+    const add_done = () => {
+      done_count += 1
+      if (done_count === envs.length) {
+        setTimeout(callback, 200)
+      }
     }
+
+    for (const env of envs) {
+      var $imgs = $('img', env), total = $imgs.length, sum = 0
+      if (total == 0) {
+        add_done()
+      } else {
+        $imgs.each(function () {
+          var img = new Image();
+          img.src = $(this).attr('src')
+          img.onload = function () {
+            sum++;
+            if (sum === total) {
+              add_done()
+            }
+          }
+        })
+      }
+    }
+  }
 })
